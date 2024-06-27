1a4e6cc5171ff640aa3462bcd01a5dcd18946491
Martin Laak <martin.laak@gmail.com>
2024-06-27 17:21:42 +0200

    intaller

---
+++ vendor/Robot32lib/Middleware/install.bash
@@ -0,0 +1,9 @@
+mkdir -p vendor/Robot32lib/Middleware
+cd vendor/Robot32lib/Middleware
+
+curl "https://raw.githubusercontent.com/mlaak/robot32lib/main/vendor/Robot32lib/Middleware/Middleware.php" > Middleware.php
+
+cd ..
+cd ..
+cd ..
+
