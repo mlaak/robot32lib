131adcd7f2e5af6bda6ae3ee7762e891972f7b06
Martin Laak <martin.laak@gmail.com>
2024-06-28 15:43:59 +0200

    GPTlib installer

---
+++ vendor/Robot32lib/GPTlib/install.bash
@@ -0,0 +1,8 @@
+mkdir -p vendor/Robot32lib/GPTlib
+cd vendor/Robot32lib/GPTlib
+
+curl "https://raw.githubusercontent.com/mlaak/robot32lib/main/vendor/Robot32lib/GPTlib/GPTlib.php" > GPTlib.php
+
+cd ..
+cd ..
+cd ..
