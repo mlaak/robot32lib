b667c022ddd50ea6dd8453c20558fc6dc5f712a6
Martin Laak <martin.laak@gmail.com>
2024-07-04 00:48:53 +0200

    changes - cammon it is 1 in the morning.

--- vendor/Robot32lib/Middleware/Middleware.php
+++ vendor/Robot32lib/Middleware/Middleware.php
@@ -12,7 +12,26 @@ if(isset($_REQUEST['r_ression_id']) || isset($_COOKIE['r_ression_id'])){
      
     if(file_exists("$BASE_DIR/working_data/sessions/$sess_id.txt")){
         $d = explode(",",file_get_contents("$BASE_DIR/working_data/sessions/$sess_id.txt"),2);
-        $_ENV['R_USER_ID'] = trim($d[0]);
-        $_ENV['R_USER_EMAIL'] = trim($d[1]);        
+        
+        $_ENV['R_USER_TYPE'] = trim($d[0]);
+        if(!ctype_alnum($_ENV['R_USER_TYPE']))$_ENV['R_USER_TYPE']="";
+                
+        $_ENV['R_USER_ID'] = trim($d[1]);
+        if(!ctype_alnum($_ENV['R_USER_ID']))$_ENV['R_USER_ID']="";
+        
+        $_ENV['R_USER_EMAIL'] = trim($d[2]);        
     }
 }
+
+if(!function_exists("rheader")){
+    function rheader(string $header, bool $replace = true, int $response_code = 0){
+        return header($header,$replace,$response_code);
+    }
+}
+
+if(!function_exists("rheaders_sent")){
+    function rheaders_sent(string &$filename = null, int &$line = null){
+        return headers_sent($filename,$line);
+    }        
+}
+
--- vendor/Robot32lib/ULogger/ULogger.php
+++ vendor/Robot32lib/ULogger/ULogger.php
@@ -2,13 +2,13 @@
 namespace Robot32lib\ULogger;
 
 class ULogger{
-   
-    function __construct(){
-
+    $base_dir = "";
+    function __construct($base_dir){
+        $this->base_dir = $base_dir;
     }
     
     function log($query,$model,$response,$response_id,$tokens_in,$tokens_out,$cost){
-        global $BASE_DIR;
+        $BASE_DIR = $this->base_dir;
         $currentTime = time();
         $year = date('Y', $currentTime);
         $month = date('m', $currentTime);
@@ -22,14 +22,14 @@ class ULogger{
         $filename = str_replace(".", "_", $filename); // replace the decimal with an underscore
         file_put_contents($BASE_DIR."/collected_data/chats/".$filename.".txt", "Model: $model\n\n"."Query:\n".$query."\n\n\nResult:\n".$response."\n\nCost:".$cost);
     
-        if(isset($_ENV['R_USER_ID'])){
+        /*if(isset($_ENV['R_USER_ID'])){
             $userid = $_ENV['R_USER_ID'];
             $l = "$currentTime,$tokens_in,$tokens_out,$response_id,$cost";
             $l = str_pad($l,$tokens_in+$tokens_out);
             $l.="\n";
             if(!ctype_alnum($userid))exit("user id must be alphanumeric");
             file_put_contents($BASE_DIR."/working_data/tokensused/$userid.txt",$l,FILE_APPEND);    
-        }
+        }*/
     
     
     
