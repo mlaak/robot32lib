50a8df2605df6386fddec233636acfa9280e82d3
mlaak <martin.laak@gmail.com>
2024-07-19 19:51:25 +0200

    LLMServerList improvement

--- vendor/Robot32lib/LLMServerList/LLMServerList.php
+++ vendor/Robot32lib/LLMServerList/LLMServerList.php
@@ -1,12 +1,19 @@
 <?php
 namespace Robot32lib\LLMServerList;
 
-//if(!in_array($model, ['mistralai/mixtral-8x7b-instruct',"mistralai/mixtral-8x22b-instruct"])){
-//    echo "Model not supported!";    
-//}
 
 class LLMServerList{
-    function getLoginFor($type){
+    function getLLMFor($type,$crud=null,$history=null,$query=null){
+        return $this->getLoginFor($type,$crud,$history,$query);
+    }    
+
+    //deprecated - use getLLMFor
+    function getLoginFor($type,$crud=null,$history=null,$query=null){
+        $size = 0;
+        if($crud!=null)$size+=strlen(var_export($crud,true));
+        if($history!=null)$size+=strlen(var_export($history,true));
+        if($query!=null)$size+=strlen(var_export($query,true));
+
         if($type=="fast"){
             $url = "https://openrouter.ai/api/v1/chat/completions"; 
             $OPENROUTER_API_KEY = $GLOBALS["OPENROUTER_API_KEY"];
@@ -15,7 +22,14 @@ class LLMServerList{
                 "Content-Type: application/json"
             ];
             $model = "mistralai/mixtral-8x7b-instruct";
-            return [["url"=>$url,"headers"=>$headers,"model"=>$model]];
+            $options =  [
+                "temperature"=> 1,  
+                "max_tokens"=> 8024,
+                "top_p"=> 1,        
+                "stream"=> true,
+                "stop"=> null];
+
+            return [["url"=>$url,"headers"=>$headers,"model"=>$model,'options'=>$options]];
         }
         else if($type=="smart"){
             $url = "https://openrouter.ai/api/v1/chat/completions";
@@ -25,7 +39,13 @@ class LLMServerList{
                 "Content-Type: application/json"
             ];
             $model = "mistralai/mixtral-8x7b-instruct";
-            return [["url"=>$url,"headers"=>$headers,"model"=>$model]];
+            $options =  [
+                "temperature"=> 1,  
+                "max_tokens"=> 8024,
+                "top_p"=> 1,        
+                "stream"=> true,
+                "stop"=> null];
+            return [["url"=>$url,"headers"=>$headers,"model"=>$model,'options'=>$options]];
 
         }
     }
