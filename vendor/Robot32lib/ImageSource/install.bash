7acb0a300b70350b852113bc83d3e41e9fc0319c
mlaak <martin.laak@gmail.com>
2024-07-20 21:47:16 +0200

    fix

--- vendor/Robot32lib/ImageSource/install.bash
+++ vendor/Robot32lib/ImageSource/install.bash
@@ -4,6 +4,7 @@ cd vendor/Robot32lib/ImageSource
 curl "https://raw.githubusercontent.com/mlaak/robot32lib/main/vendor/Robot32lib/ImageSource/ImageSource.php" > ImageSource.php
 curl "https://raw.githubusercontent.com/mlaak/robot32lib/main/vendor/Robot32lib/ImageSource/pictures.archive" > pictures.archive
 curl "https://raw.githubusercontent.com/mlaak/robot32lib/main/vendor/Robot32lib/ImageSource/pictures.locations.txt" > pictures.locations.txt
+curl "https://raw.githubusercontent.com/mlaak/robot32lib/main/vendor/Robot32lib/ImageSource/LICENSE.txt" > LICENSE.txt
 
 cd ..
 cd ..
