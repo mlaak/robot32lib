17d7c22085d8aa06219e90b740c25087911b12d2
mlaak <martin.laak@gmail.com>
2024-07-20 18:09:43 +0200

    ClassTree v0.2

--- vendor/Robot32lib/ClassTree/install.bash
+++ vendor/Robot32lib/ClassTree/install.bash
@@ -2,6 +2,7 @@ mkdir -p vendor/Robot32lib/ClassTree
 cd vendor/Robot32lib/ClassTree
 
 curl "https://raw.githubusercontent.com/mlaak/robot32lib/main/vendor/Robot32lib/ClassTree/ClassTree.php" > ClassTree.php
+curl "https://raw.githubusercontent.com/mlaak/robot32lib/main/vendor/Robot32lib/ClassTree/classtree.txt" > classtree.txt
 
 cd ..
 cd ..
